ee069ea8ec85b2d2fdb257323acfbc3ff49a2e3a
Nikolay Nikolov <nikolay.s.nikolov@gmail.com>
2024-06-25 09:39:54 +0300

    Added more solutions

--- 11_table_relations/11_table_relations.sql
+++ 11_table_relations/11_table_relations.sql
@@ -1,3 +1,5 @@
+-- Task 1
+
 CREATE TABLE mountains
 (
     id   SERIAL PRIMARY KEY,
@@ -12,3 +14,38 @@ CREATE TABLE peaks
         FOREIGN KEY (mountain_id)
             REFERENCES mountains (id)
 );
+
+--Task 2
+
+SELECT v.driver_id,
+       v.vehicle_type,
+       CONCAT(c.first_name, ' ', c.last_name) AS driver_name
+FROM vehicles AS v
+         JOIN campers AS c ON v.driver_id = c.id;
+
+-- Task 3
+
+SELECT
+	r.start_point,
+	r.end_point,
+	r.leader_id,
+	CONCAT(c.first_name, ' ', c.last_name) AS leader_name
+FROM routes AS r JOIN campers AS c ON c.id = r.leader_id;
+
+-- Task 4
+
+CREATE TABLE mountains(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
+);
+CREATE TABLE peaks(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	mountain_id INT,
+	CONSTRAINT fk_mountain_id
+		FOREIGN KEY(mountain_id)
+			REFERENCES mountains(id)
+				ON DELETE CASCADE
+);
+
+
